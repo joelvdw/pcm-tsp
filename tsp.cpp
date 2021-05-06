7391be8ac8d3b9aae42bdded5cfb27c278b9faa9
joel.vonderwe <joel.vonderweid@master.hes-so.ch>
2021-05-06 10:58:57 +0200

    Implement threadpool and improve duration calcul

--- tsp.cpp
+++ tsp.cpp
@@ -5,6 +5,7 @@
 #include <math.h>
 #include <sys/times.h>
 #include <time.h>
+#include <iostream>
 
 #include "graph.h"
 #include "path.h"
@@ -12,39 +13,19 @@
 #include "ConcurrentReuseQueue.h"
 #include "ThreadPool.h"
 
-#define SHORTEST_LOCAL false
+using namespace std::chrono;
 
+#define SHORTEST_LOCAL false
+#define DEFAULT_THREADS 8
+
+path_t shortest_global;
 #if SHORTEST_LOCAL
-thread_local path_t* shortest_thread;
-#else
-path_t* shortest_global;
+thread_local path_t shortest_thread;
 #endif
 
 path_t end = { -1, -1, -1, -1, NULL };
 long* counters;
 
-static void task(int id, int nbthreads, graph_t* g, ConcurrentReuseQueue<path_t>* queue) {
-  path_t* current;
-  while (true) {
-    while ((current = queue->dequeue()) != NULL) {
-      if (current->size == -1) {
-        break;
-      }
-
-#if SHORTEST_LOCAL
-      branch_and_bound(g, current, shortest_thread, counters);
-#else
-      branch_and_bound(g, current, shortest_global, counters);
-#endif
-    }
-
-    if (current->size == -1) {
-      break;
-    }
-
-    // wait cv
-  }
-}
 
 static void branch_and_bound(graph_t *g, path_t *current, path_t *shortest, long* counters)
 {
@@ -60,7 +41,7 @@ static void branch_and_bound(graph_t *g, path_t *current, path_t *shortest, long
       // continue branching
       for (i=1; i<size; i++) {
         if (path_add_node(current, i, g, 1)) {
-          branch_and_bound(g, current, shortest, counters, verbose);
+          branch_and_bound(g, current, shortest, counters);
           path_drop_tail(current, g);
         }
       }
@@ -77,10 +58,41 @@ static void branch_and_bound(graph_t *g, path_t *current, path_t *shortest, long
 }
 
 
+static void task(int id, int nbthreads, graph_t* g, ConcurrentReuseQueue<path_t>* queue) {
+  if (id) { /* Remove warning */ }
+
+  // TODO init shortest local
+
+  path_t* current;
+  while (true) {
+    while ((current = queue->dequeue()) != NULL) {
+      if (current->size == -1) {
+        break;
+      }
+
+#if SHORTEST_LOCAL
+      branch_and_bound(g, current, &shortest_thread, counters);
+#else
+      branch_and_bound(g, current, &shortest_global, counters);
+#endif
+    }
+
+    if (current->size == -1) {
+      break;
+    }
+
+    // wait cv
+    if (nbthreads) {}
+  }
+
+  // TODO merge shortest local to global
+}
+
+
 static void check_argv(int argc, char *argv[])
 {
-  if (!(argc == 2 || (argc == 3 && !strcmp(argv[1], "-v")))) {
-    fprintf(stderr, "usage: %s [-v] filename\n", argv[0]);
+  if (!(argc == 2 || (argc == 3 && atoi(argv[2]) > 0))) {
+    fprintf(stderr, "usage: %s filename [numberOfThreads]\n", argv[0]);
     exit(1);
   }
 }
@@ -102,20 +114,15 @@ static long *alloc_counters(int size, char *pname)
 int main(int argc, char *argv[])
 {
   graph_t graph;
-  path_t current, shortest;
+  path_t current;
   struct tms tb;
-  time_t start;
-  long *counters;
   int size;
+  int nbThread = DEFAULT_THREADS;
   char *fname = argv[1];
-  char verbose = 0;
-
-  start = time(0);
 
   check_argv(argc, argv);
   if (argc == 3) {
-    verbose = 1;
-    fname = argv[2];
+    nbThread = atoi(argv[2]);
   }
   graph_read_tsplib(&graph, fname);
   graph_print(&graph);
@@ -123,21 +130,39 @@ int main(int argc, char *argv[])
   size = graph_size(&graph);
   counters = alloc_counters(size+1, argv[0]);
 
-  path_new(&shortest, size+1);
-  path_add_all_nodes(&shortest, &graph);
-  path_add_node(&shortest, 0, &graph, 0);
+  path_new(&shortest_global, size+1);
+  path_add_all_nodes(&shortest_global, &graph);
+  path_add_node(&shortest_global, 0, &graph, 0);
 
   path_new(&current, size+1);
   path_add_node(&current, 0, &graph, 0);
 
-  branch_and_bound(&graph, &current, &shortest, counters, verbose);
-  path_print(&shortest, (char*)"shortest");
+  ConcurrentReuseQueue<path_t>* queue = new ConcurrentReuseQueue<path_t>();
+  queue->enqueue(&current);
+  queue->enqueue(&end);
 
+  const auto start = system_clock::now();
+
+  ThreadPool* pool = new ThreadPool(task, nbThread, &graph, queue);
+  pool->joinAll();
+
+  long ms = duration_cast< milliseconds >(
+      (system_clock::now() - start)
+  ).count();
   times(&tb);
-  printf("elapsed time: %lds\n", time(0) - start);
-  printf("total CPU time: %lds\n", (tb.tms_utime + tb.tms_stime)/CLOCKS_PER_SEC);
+
+  path_print(&shortest_global, (char*)"shortest");
+
+  printf("elapsed time: %.4fs\n", (float)ms/1000.0f);
+  printf("total CPU time: %.4fs\n", (float)(tb.tms_utime + tb.tms_stime)/CLOCKS_PER_SEC);
   printf("total paths bound/checks:");
   for (int i = 0; i <= size ; i++)
     printf(" %ld", counters[i]);
   printf("\n");
+
+  free(counters);
+  pool->free();
+  queue->close();
+  delete pool;
+  delete queue; 
 }
